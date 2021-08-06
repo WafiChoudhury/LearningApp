e153d134474527f2fc23884586d4fbf7e74b430a
Wafi Choudhury <wafster1337@gmail.com>
2021-08-06 01:26:03 -0500

    Set Up

---
+++ LearningApp/ViewModels/ContentModel.swift
@@ -0,0 +1,18 @@
+//
+//  ContentModel.swift
+//  LearningApp
+//
+//  Created by Wafi Choudhury on 8/6/21.
+//
+
+import Foundation
+
+
+class ContentModel: ObservableObject{
+    
+    
+    
+    
+    
+}
+
